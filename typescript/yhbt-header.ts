f82f8539d1d60857814cb37df04e5d6f06b0cac8
Mika Turunen <mika.turunen@ymail.com>
2015-06-03 19:07:21 +0300

    Base commit of all the component parts.

---
+++ typescript/yhbt-header.ts
@@ -0,0 +1,14 @@
+
+Polymer({
+    /**
+     * What component we are attached to.
+     */
+    is: "yhbt-header",
+
+    /**
+     * Explaining the properties of the component
+     */
+    properties: {
+
+    }
+});
